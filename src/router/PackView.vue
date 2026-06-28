f03c702defbde841dea6386d4ef29fd41d4a1e30
PashaParfeni <dev72591@outlook.com>
2026-06-28 17:59:41 +0330

    creating matrix room state event

--- src/router/PackView.vue
+++ src/router/PackView.vue
@@ -3,18 +3,46 @@
 <template>
   <div class="header-2">
     {{ title }}
-    <a class="add-btn" :href="link">add {{ typeName }} to telegram</a>
+    <a class="add-btn telegram" :href="link" style="margin-right: 0.5em">add to telegram</a>
+    <a class="add-btn matrix" v-on:click="isMatrixPopupOpen = true">add to matrix</a>
   </div>
   <StickerGrid :cards="stickers" size="large" />
   <template v-if="premium_stickers.length > 0">
     <div class="header-2 smaller">premium animation</div>
     <StickerGrid :cards="premium_stickers" size="large" />
   </template>
+  <template v-if="isMatrixPopupOpen">
+    <div class="popup-container">
+      <div class="popup">
+        {{ matrixEvent }}
+      </div>
+    </div>
+  </template>
 </template>
 
 <script>
 import { fetchJson } from '@/utils'
 import StickerGrid from '@/components/StickerGrid.vue'
+
+function extToMimetype(ext) {
+  switch (ext.toLowerCase()) {
+    case 'png':
+      return 'image/png'
+    case 'jpg':
+    case 'jpeg':
+      return 'image/jpeg'
+    case 'webp':
+      return 'image/webp'
+    case 'webm':
+      return 'video/webm'
+    case 'gif':
+      return 'image/gif'
+    case 'tgs':
+      return 'video/tgs'
+    default:
+      return undefined
+  }
+}
 
 export default {
   components: { StickerGrid },
@@ -23,12 +51,16 @@ export default {
       title: '',
       link: '',
       typeName: '',
+      matrixEvent: '',
+      isMatrixPopupOpen: false,
       stickers: [],
       premium_stickers: [],
     }
   },
   async created() {
-    const data = await fetchJson(`https://sticker-repo.github.io/s1/info/${this.$route.params.packName}.json`)
+    const data = await fetchJson(
+      `https://sticker-repo.github.io/s1/info/${this.$route.params.packName}.json`,
+    )
     if (!data) return
     this.title = data.title
     if (data.sticker_type === 'regular') {
@@ -38,7 +70,16 @@ export default {
       this.link = `https://t.me/addemoji/${data.name}`
       this.typeName = 'emojis'
     }
-    data.stickers.forEach((sticker) => {
+    const matrixEvent = {
+      images: {},
+      pack: {
+        display_name: data.name,
+        attribution: `[sticker-repo.github.io] Original pack at ${this.link}`,
+        usage: ['sticker', 'emoticon'],
+      },
+    }
+    for (let i = 0; i < data.stickers.length; i++) {
+      const sticker = data.stickers[i]
       let s = {
         id: sticker.file_unique_id,
         src: `https://sticker-repo.github.io/s1/files/${this.$route.params.packName}/${sticker.file_unique_id}.${sticker.extension}`,
@@ -49,8 +90,19 @@ export default {
         this.premium_stickers.push(s)
       } else {
         this.stickers.push(s)
+        matrixEvent.images[String(i)] = {
+          url: `mxc://worker.sticker-repo.workers.dev/s1-${data.name}-${sticker.file_unique_id}-${sticker.extension}`,
+          body: sticker.emoji,
+        }
+        const mimetype = extToMimetype(sticker.extension)
+        if (mimetype) {
+          matrixEvent.images[String(i)]['info'] = {
+            mimetype: mimetype,
+          }
+        }
       }
-    })
+    }
+    this.matrixEvent = JSON.stringify(matrixEvent);
   },
 }
 </script>
