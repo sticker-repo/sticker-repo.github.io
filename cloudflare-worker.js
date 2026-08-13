89bfd16fb4498c0c327c0b63852baf31540301d5
PashaParfeni <dev72591@outlook.com>
2026-08-13 15:53:10 +0330

    Update cloudflare-worker.js

--- cloudflare-worker.js
+++ cloudflare-worker.js
@@ -43,7 +43,7 @@ async function getFileBytes(mediaId) {
       contentType = "image/gif";
       break;
     case "tgs":
-      contentType = "image/tgs";
+      contentType = "application/x-tgsticker";
       break;
     default:
       contentType = res.headers.get("content-type") || "application/octet-stream";
